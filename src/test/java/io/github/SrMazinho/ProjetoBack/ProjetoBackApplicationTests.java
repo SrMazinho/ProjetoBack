b0ac00f5eeeeae6f9df9aef0166d446587593b67
SrMazinho <florst4123@gmail.com>
2023-09-25 16:38:28 -0300

    Initial Commit

---
+++ src/test/java/io/github/SrMazinho/ProjetoBack/ProjetoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.SrMazinho.ProjetoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
